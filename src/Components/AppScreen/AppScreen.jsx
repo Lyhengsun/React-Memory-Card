7cda46d42103a7b1c8a119c256ee652d178c7218
Lyhengsun <hengsun2.168@gmail.com>
2024-09-14 12:59:16 +0700

    clean comments and some logs

--- src/Components/AppScreen/AppScreen.jsx
+++ src/Components/AppScreen/AppScreen.jsx
@@ -18,38 +18,14 @@ function AppScreen() {
         }
       })
       .map((pokemon) => pokemon.id);
-    //const maxPokemon = 9;
-    //const maxSelectedPokemon = selectedIds.length < 3 ? selectedIds.length : 3;
-    //for (let index = 0; index < maxPokemon; index++) {
-    //  if (index + 1 <= maxSelectedPokemon) {
-    //    const randomIndex = Math.floor(Math.random() * selectedIdsCopy.length);
-    //    pokemons.push(pokemonData[selectedIdsCopy[randomIndex]]);
-    //    selectedIdsCopy.splice(randomIndex, 1);
-    //  } else {
-    //    pokemons.push(pokemonData[unSelectedIds[0] - 1]);
-    //    unSelectedIds.splice(0, 1);
-    //  }
-    //}
-    console.log("selectedIds");
-    console.log(selectedIdsCopy);
-    console.log("unSelectedIds");
-    console.log(unSelectedIds);
+
+    //console.log("selectedIds");
+    //console.log(selectedIdsCopy);
+    //console.log("unSelectedIds");
+    //console.log(unSelectedIds);
 
     let testindex = 0;
-    //for (let index = 0; index < 9; index++) {
-    //  console.log(selectedIds.includes(pokemonData[index].id));
-    //  console.log(pokemons.length);
-    //  if (!selectedIds.includes(pokemonData[index].id)) {
-    //    pokemons.push(pokemonData[index]);
-    //  }
-    //}
     while (pokemons.length < 9) {
-      //console.log(!selectedIds.includes(pokemonData[testindex].id));
-      //if (!selectedIds.includes(pokemonData[testindex].id)) {
-      //  pokemons.push(pokemonData[testindex]);
-      //}
-      //testindex++;
-
       if (
         selectedIds.length === pokemonData.length ||
         (pokemons.length < 3 && selectedIdsCopy.length > 0)
@@ -61,19 +37,14 @@ function AppScreen() {
         pokemons.push(pokemonData[unSelectedIds[testindex] - 1]);
         testindex++;
       }
-      //}
     }
     return pokemons;
   }
 
   const visiblePokemon = getVisiblePokemon();
-  console.log("selectedIds");
-  console.log(selectedIds);
-
-  console.log("visiblePokemon");
-  console.log(visiblePokemon);
-  console.log(pokemonData);
-  //console.log("rendering");
+  //console.log("visiblePokemon");
+  //console.log(visiblePokemon);
+  console.log("rendering");
 
   return (
     <div
