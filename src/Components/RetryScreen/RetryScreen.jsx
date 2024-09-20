09d317f2df6f7f59af33c1653d742198e3f7f0f1
Lyhengsun <hengsun2.168@gmail.com>
2024-09-20 19:15:01 +0700

    added some styles in retryscren

--- src/Components/RetryScreen/RetryScreen.jsx
+++ src/Components/RetryScreen/RetryScreen.jsx
@@ -13,9 +13,41 @@ function RetryScreen({ setAppState = () => {} }) {
 
   return (
     <div>
-      <div>RetryScreen</div>
-      <button onClick={handleOnClickRetryBtn}>Retry</button> <br />
-      <button onClick={handleOnClickMenuBtn}>Back to menu</button>
+      <div
+        style={{ fontWeight: "bold", fontSize: "3em", marginBottom: "10px" }}
+      >
+        You Fail!
+      </div>
+      <button
+        onClick={handleOnClickRetryBtn}
+        style={{
+          border: "none",
+          padding: "12px 24px",
+          borderRadius: "15px",
+          backgroundColor: "lime",
+          color: "white",
+          fontWeight: "bold",
+          fontSize: "1.5em",
+          marginBottom: "20px",
+        }}
+      >
+        Retry
+      </button>{" "}
+      <br />
+      <button
+        onClick={handleOnClickMenuBtn}
+        style={{
+          border: "none",
+          padding: "12px 24px",
+          borderRadius: "15px",
+          backgroundColor: "lime",
+          color: "white",
+          fontWeight: "bold",
+          fontSize: "1.5em",
+        }}
+      >
+        Back to menu
+      </button>
     </div>
   );
 }
