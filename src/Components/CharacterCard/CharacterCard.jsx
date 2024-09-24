ff60823139be21901c51b3c3707bcae28a779c44
Lyhengsun <hengsun2.168@gmail.com>
2024-09-24 19:35:44 +0700

    added parallax card effect and back card with 3d flip

--- src/Components/CharacterCard/CharacterCard.jsx
+++ src/Components/CharacterCard/CharacterCard.jsx
@@ -4,6 +4,8 @@ import styles from "./CharacterCard.module.css";
 import { capitalize } from "../../Utils";
 import PropTypes from "prop-types";
 import CharacterModel from "../../Models/CharacterModel";
+import Tilt from "react-parallax-tilt";
+import CardBackImg from "../../assets/card-back.png";
 
 function CharacterCard({
   pokemonData,
@@ -30,28 +32,47 @@ function CharacterCard({
   }
 
   return (
-    <div
+    <Tilt
+      tiltReverse
+      reset
+      glareEnable={true}
       className={styles.CharacterCardContainer}
-      onClick={handleOnClick}
-      style={{ ...style, padding: longerScreenWidth ? "1vh" : "1vw" }}
     >
       <div
-        className={styles.CharacterCardImage}
-        style={{ width: longerScreenWidth ? "18vh" : "27vw" }}
+        className={styles.CharacterCardInner}
+        onClick={handleOnClick}
+        style={{
+          ...style,
+          aspectRatio: "2.5/3.5",
+          width: longerScreenWidth ? "18vh" : "24vw",
+        }}
       >
-        <img
-          src={imgUrl}
-          alt={name + ".png"}
-          style={{ width: "100%", height: "100%" }}
-        />
+        <div
+          className={styles.CharacterCardFront}
+          style={{
+            padding: longerScreenWidth ? "1vh" : "1vw",
+          }}
+        >
+          <div className={styles.CharacterCardImage}>
+            <img
+              src={imgUrl}
+              alt={name + ".png"}
+              style={{ width: "100%", height: "100%" }}
+              draggable={false}
+            />
+          </div>
+          <div
+            className={styles.CharacterCardName}
+            style={{ fontSize: longerScreenWidth ? "2vh" : "3vw" }}
+          >
+            <div style={{ textAlign: "center" }}>{capitalize(name)}</div>
+          </div>
+        </div>
+        <div className={styles.CharacterCardBack}>
+          <img src={CardBackImg} style={{ width: "109%" }} alt="" />
+        </div>
       </div>
-      <div
-        className={styles.CharacterCardName}
-        style={{ fontSize: longerScreenWidth ? "2vh" : "3vw" }}
-      >
-        {capitalize(name)}
-      </div>
-    </div>
+    </Tilt>
   );
 }
 CharacterCard.propTypes = {
